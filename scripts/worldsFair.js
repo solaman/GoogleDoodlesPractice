18afee72b89feb01acd7ca66fc87df33f0bd711b
solaman <soulhuq@outlook.com>
2016-05-28 18:26:21 -0500

    Centered a little bit.

--- scripts/worldsFair.js
+++ scripts/worldsFair.js
@@ -4,40 +4,33 @@
 (function() {
    var doodleContainerElement = document.getElementById("doodle-container").getElementsByClassName("overlay")[0];
    var lookingGlassElement = document.getElementById("looking-glass");
-   var smallDoodleWidth = doodleContainerElement.clientWidth;
 
    var bigDoodleContainerElement = document.getElementById("big-doodle-container-mid");
-   //var bigDoodleContainerTopElement = document.getElementById("big-doodle-container-top");
-   //var bigDoodleContainerBottomElement = document.getElementById("big-doodle-container-bottom");
-
    var bigDoodleElement = bigDoodleContainerElement.getElementsByTagName("img")[0];
-   var bigDoodleWidth = bigDoodleElement.naturalWidth;
-   var doodleDisplacement = (bigDoodleWidth/ smallDoodleWidth - 1);
+   var doodleRatioWidth = (bigDoodleElement.naturalWidth/document.getElementById("base-doodle").clientWidth);
+   var doodleRatioHeight = (bigDoodleElement.naturalHeight/document.getElementById("base-doodle").clientHeight);
 
-   //var bigDoodleTopElement = bigDoodleContainerTopElement.getElementsByTagName("img")[0];
-   //var bigDoodleBottomElement = bigDoodleContainerBottomElement.getElementsByTagName("img")[0];
 
-    var actionCount = 0;
-    var currentAction = 0;
    function moveLookingGlass(event) {
        var x = event.clientX;
        var y = event.clientY;
 
-       lookingGlassElement.style["top"] = (y-50) + "px";
-       lookingGlassElement.style["left"] = (x- 50) + "px";
+       lookingGlassElement.style["top"] = (y-100) + "px";
+       lookingGlassElement.style["left"] = (x- 100) + "px";
    }
 
    function moveMagnifiedDoodle(event) {
-       var x = event.clientX;
-       var y = event.clientY;
+       var x = event.clientX - 100;
+       var y = event.clientY - 100;
 
-       var deltaX = x*doodleDisplacement;
-       var deltaY = y*doodleDisplacement;
+       var fixedDeltaX = (x)*doodleRatioWidth/2;
+       var fixedDeltaY = (y)*doodleRatioHeight/2;
 
-       bigDoodleElement.style["top"] = -(y + deltaY) + "px";
-       bigDoodleElement.style["left"] = -(x + deltaX) + "px";
-       bigDoodleContainerElement.style["top"] = (y - 46) + "px";
-       bigDoodleContainerElement.style["left"] = (x - 46) + "px";
+       bigDoodleContainerElement.style["top"] = y + 2 + "px";
+       bigDoodleContainerElement.style["left"] = x + 2 + "px";
+
+       bigDoodleElement.style["top"] = 100 - y - fixedDeltaY + "px";//-(y-100) +(deltaY + 100) + "px";
+       bigDoodleElement.style["left"] = 100 - x - fixedDeltaX + "px";
 
    }
 
